716e28366575d7354a3582c207325449fb089ff2
vickyhovey <vickyhovey@gmail.com>
2018-06-12 23:29:48 -0400

    react-router

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -9,16 +9,26 @@ const ExpenseDashboardPage = () => (
         This is from my dashboard component
     </div>
 );
+
 const AddExpensePage = () => (
   <div>This is from my dashboard component</div>
+);
+
+const EditExpensePage = () => (
+  <div>This is from my EditExpensePage component</div>
+);
+
+const HelpPage = () => (
+    <div>This is from my help component</div>
 );
 
 const routes = (
   <BrowserRouter>
     <div>
-        <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-        <Route path="/create" component={AddExpensePage}/>
-
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage}/> 
+      <Route path="/help" component={HelpPage}/>
     </div>
   </BrowserRouter>
 );
