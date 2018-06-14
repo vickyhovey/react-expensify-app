c58bc7b71d37f6b743a0a50743ad3d7120fea216
vickyhovey <vickyhovey@gmail.com>
2018-06-13 23:26:58 -0400

    Query strings and url parameters

--- expensify-app/src/components/EditExpensePage.js
+++ expensify-app/src/components/EditExpensePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const EditExpensePage = () => (
+const EditExpensePage = (props) => {
+  console.log(props);
+  return (
   <div>
-    This is from my edit expense component
+    Editing the expense with id of {props.match.params.id}
   </div>
 );
-
+};
 export default EditExpensePage;
